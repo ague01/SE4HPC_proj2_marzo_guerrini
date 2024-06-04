c2a28e294ee7e93d14c60237f45d0778a58b49ea
ague01 <aguerrini01@gmail.com>
2024-06-04 10:09:09 +0000

    added run bash script and minor bugs

--- run_script.sh
+++ run_script.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-#SBATCH --job-name singularity-mpi
-#SBATCH -N $NNODES # total number of nodes
-#SBATCH --time=00:05:00 # Max execution time
+#SBATCH --job-name=singularity-mpi
+#SBATCH --nodes=1
+#SBATCH --ntasks=2
+#SBATCH --time=00:05:00
+#SBATCH --output=output_file.txt
+#SBATCH --partition=g100_all_serial
 
-mpirun -n $NP singularity exec /var/nfsshare/gvallee/mpich.sif /app/build/main
+module load openmpi
+
+export HWLOC_COMPONENTS=-gl
+export OMPI_MCA_btl=^openib
+
+mpirun -n 2 singularity run mat_mul
